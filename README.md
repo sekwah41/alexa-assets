64551baa04a26010ab0cf54bd7811a828647fa9d
Sekwah <sekwah41@gmail.com>
2018-05-29 21:37:53 +0100

    Added output speech

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Simple assets for making rest responses to Alexa, the purpose of this project is to create objects and documentation to help with creating responses to make simple or complex skills easier.
 
+Also for easier reading of the data sent by amazon due to markup though if you have taken a look at the json formatting it should be straight forward.
+
 Uses language version ECMAScript 6 as that is what I am using generally for NodeJS atm.
 
 I will not work on this right now but it is a project I will do. This is mostly here as a test npm repo to make sure its set up right
+
+Possibly will make a constructor to help create SSML messages
--- index.js
+++ index.js
@@ -1,6 +1,59 @@
 "use strict";
 
+/**
+ * true for plaintext, false for ssml
+ */
+class OutputSpeech {
+    constructor(isPlainText, content) {
+        if(isPlainText) {
+            this.type = "PlainText";
+            this.text = content;
+        }
+        else {
+            this.type = "SSML";
+            this.ssml = content;
+        }
+    }
+    getType() {
+        return this.type;
+    }
+    getContents() {
+        return this.type === "PlainText" ? this.text : this.ssml;
+    }
+}
 
+/**
+ * Used to be inherited by the other types of card however you can use it to custom create one if you really want
+ */
+class Card {
+    constructor(type) {
+        this.type = type;
+    }
+    getType() {
+        return this.type;
+    }
+}
+exports.Card = Card;
 
-module.exports = {};
+/**
+ * A card that contains a title and plain text content
+ */
+class SimpleCard extends Card {
+    /**
+     * @param title of card
+     * @param text of card
+     */
+    constructor(title, text) {
+        super("Simple");
+        this.title = title;
+        this.text = text;
+    }
+    getTitle() {
+        return this.title;
+    }
+    getText() {
+        return this.text;
+    }
+}
+exports.SimpleCard = SimpleCard;
 
